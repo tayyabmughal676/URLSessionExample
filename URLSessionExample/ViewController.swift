06ce52361963cf1ba7c50a4daf490f5ce70cea5c
tayyabmughal676 <tayyabmughal676@gmail.com>
2021-07-02 13:42:34 +0500

    Initial Commit

---
+++ URLSessionExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  URLSessionExample
+//
+//  Created by Thor on 02/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
